abc3bb9b608df9d9b6504f6c70dd71ab9852172e
Leo Mazza <leomazza@Leos-MacBook-Air.local>
2024-01-11 12:29:51 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ionic-segment-bug',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
